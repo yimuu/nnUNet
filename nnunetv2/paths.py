c8e9e0338a64e03841f465d601c4cc84e60efcb6
yimuu <yimoo12138@gmail.com>
2024-01-15 12:28:57 +0800

    update path.py

--- nnunetv2/paths.py
+++ nnunetv2/paths.py
@@ -18,6 +18,10 @@ import os
 PLEASE READ paths.md FOR INFORMATION TO HOW TO SET THIS UP
 """
 
+os.environ['nnUNet_raw'] = '/home/wyh/Codes/nnUNetJAX/nnunetv2/data/nnUNet_raw'
+os.environ['nnUNet_preprocessed'] = '/home/wyh/Codes/nnUNetJAX/nnunetv2/data/nnUNet_preprocessed'
+os.environ['nnUNet_results'] = '/home/wyh/Codes/nnUNetJAX/nnunetv2/results'
+
 nnUNet_raw = os.environ.get('nnUNet_raw')
 nnUNet_preprocessed = os.environ.get('nnUNet_preprocessed')
 nnUNet_results = os.environ.get('nnUNet_results')
